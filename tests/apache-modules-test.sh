f86d3fa6b7be1d4184da9b19490b52351e6a6f6c
Cory Collier <corycollier@corycollier.com>
2018-12-30 09:45:29 -0500

    adding test files

---
+++ tests/apache-modules-test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+# set -o pipefail
+# set -o xtrace
+
+# First, lets do some sanity checking
+if [[ $(echo "$@" | wc -w) -lt "1" ]]; then
+  echo "[ERROR] This script requires 1 args: [docker container name]"
+  exit 1
+fi
+
+declare _name="${1}"
+declare _actual_modules=
+declare _check_modules=(
+  "rewrite"
+  "php7_module"
+)
+
+# Store the actual modules in a local variable
+_actual_modules=$(docker exec "${_name}" bash -c "apachectl -M 2>&1")
+
+# Iterate over our check modules to make sure they're in the container
+for _module in ${_check_modules[@]}; do
+  if [[ -n $(echo "${_actual_modules}" | grep "${_module}") ]]; then
+    echo "[PASS] - apache has module [${_module}]"
+  else
+    echo "[ERROR] - module [${_module}] not found"
+  fi
+done
---
+++ tests/system-git-test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace
+
+# First, lets do some sanity checking
+if [[ $(echo "$@" | wc -w) -lt "1" ]]; then
+  echo "[ERROR] This script requires 1 args: [docker container name] "
+  exit 1
+fi
+
+declare _name="${1}"
+declare _result=
+
+_result=$(docker exec "${_name}" bash -c "which git | grep git")
+
+# If the actual version is our checked version, print a happy message
+if [[ -n "${_result}" ]]; then
+  echo "passed"
+
+# If not though, get mad
+else
+  echo "[ERROR] git not found"
+  exit 1
+fi
